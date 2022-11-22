3c5ca389734ef147057605f552389e11e8769734
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-11-22 12:17:21 -0300

    Creating connection with mysql

---
+++ database/connection.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('thoughts', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+try{
+    sequelize.authenticate()
+    console.log("Conectamos com sucesso!")
+}
+catch(err){
+    console.log("Não foi possível se conectar " + err)
+}
+module.exports = sequelize
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const express_handlebars = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('express-flash');
+const connection = require('./database/connection'); 
+
+const app = express();
+
+
+
+connection.sync()
+    .then(() => {app.listen(3000)})
+    .catch((error) => console.log(error));
